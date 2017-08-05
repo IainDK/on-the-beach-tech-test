4d6db6e0f3b4109d576f9789be608f4c749617c1
Iain Kinnish <iainkinnish@googlemail.com>
2017-08-05 14:58:58 +0100

    4th requirement met. All tests passing. Time to refactor.

--- lib/job_processor.rb
+++ lib/job_processor.rb
@@ -2,13 +2,31 @@ class JobProcessor
 
   def order_jobs(jobs)
     return "" if jobs.empty?
-    string_to_array_filter(jobs)
+    ordered_job_array(jobs)
   end
 
   private
 
-  def string_to_array_filter(jobs)
+  def string_to_array(jobs)
     array_of_jobs = jobs.split(",")
-    array_of_jobs.map { |job| job.lstrip[0] }.join("")
+    array_of_jobs.map { |job| job.lstrip }
+  end
+
+  def job_hash(jobs)
+    all_jobs = {}
+    string_to_array(jobs).each do |job|
+      all_jobs[job[0]] = job[-1]
+    end
+    all_jobs
+  end
+
+  def ordered_job_array(jobs)
+    job_array = []
+    dependencies = job_hash(jobs).values
+    job_array << dependencies
+    job_hash(jobs).keys.each do |key|
+      job_array << key unless job_array.flatten.include?(key)
+    end
+    job_array.flatten.reject { |el| el == " " }.join("")
   end
 end
--- spec/job_processor_spec.rb
+++ spec/job_processor_spec.rb
@@ -2,18 +2,30 @@ require 'job_processor'
 
 describe JobProcessor do
 
-  describe 'order_jobs' do
+  describe 'order_jobs(jobs)' do
 
     it 'returns an empty string when passed an empty string (no jobs)' do
       expect(subject.order_jobs("")).to eq ""
     end
 
     it 'returns a single job when passed a single job (a)' do
-      expect(subject.order_jobs("a =>")).to eq "a"
+      expect(subject.order_jobs("a => ")).to eq "a"
     end
 
     it 'returns all jobs in any order when given multiple jobs without dependencies (abc)' do
       expect(subject.order_jobs("a => , b => , c => ")).to eq "abc"
+    end
+
+    it 'returns a single job with a dependency in order' do
+      expect(subject.order_jobs("a => , b => c, c => ")).to eq "cab"
+    end
+
+    it 'returns a single job with a dependency in order - 2' do
+      expect(subject.order_jobs("a => b, b => , c => ")).to eq "bac"
+    end
+
+    it 'returns a single job with a dependency in order - 3' do
+      expect(subject.order_jobs("a => , b => , c => a")).to eq "abc"
     end
   end
 end
